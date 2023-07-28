87d8d8311a75ec6105e4b5c988d936657a7871b7
LiamEdwards931 <>
2023-07-28 19:12:44 +0000

    add update worksheet function that updates the google sheets with the values entered on the terminal

--- run.py
+++ run.py
@@ -12,23 +12,29 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+
 def get_sales_data():
     """
-    requests input from the user for the sales of sandwiches
+    Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
     """
     while True:
-        print("please input sales data from the last market")
-        print("Data should be 6 numbers, seperated by commas")
-        print("Example: 1,2,3,4,5,6\n")
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
         data_str = input("Enter your data here: ")
+
         sales_data = data_str.split(",")
-        
+
         if validate_data(sales_data):
-            print("Data is valid")
+            print("Data is valid!")
             break
 
-        return sales_data
+    return sales_data
+
 
 def validate_data(values):
     """
@@ -45,7 +51,20 @@ def validate_data(values):
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False
-    
+
     return True
 
+
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
+
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
