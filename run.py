2a5e0a3badc7af8ceb3a7813ea3ce5bbc8d644fa
LiamEdwards931 <>
2023-07-30 09:20:00 +0000

    add function to calculate average, add 10% and return to the love sandwiches worksheet

--- run.py
+++ run.py
@@ -90,17 +90,30 @@ def calculate_surplus_data(sales_row):
 def get_last_5_sales():
     """
     gets the last 5 entries of sales 
-    calculates the average 
-    adds 10% of the sales as an integer
+    as a list in a list
     """
     sales = SHEET.worksheet("sales")
-    
+  
     columns = []
     for ind in range(1, 7):
         column = sales.col_values(ind)
         columns.append(column[-5:])
     return columns
 
+
+def calculate_stock_data(stock):
+    """
+    gets the average of the last 5 entries of stock
+    """
+    print("Calculating Stock Data..\n")
+    new_stock_data = []
+    for column in stock:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+    
+    return  new_stock_data
 
 def main():
     """
@@ -111,8 +124,9 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
-
+    sales_columns = get_last_5_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
-sales_columns = get_last_5_sales()
